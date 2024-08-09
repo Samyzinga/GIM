be0120afe7b7b2f2bc0eebba89be54dab4eda7de
Samyzinga <samantazingariello@gmail.com>
2024-08-09 21:03:35 +0200

    a caso

--- Esercizio_3A/esercizio 3A_1/sketch.js
+++ Esercizio_3A/esercizio 3A_1/sketch.js
@@ -1,12 +1,11 @@
-let secondColor;
 let minuteColor;
 let hourColor;
 let lastSecond;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  angleMode(RADIANS);  // Assicurati che l'angolo sia in radianti
-  noStroke();  // Rimuovi il contorno
+  angleMode(RADIANS);  // angolo in radianti
+  noStroke();  // no contorno
   changeColors();  // Inizializza i colori
   lastSecond = second();  // Inizializza l'ultimo secondo
 }
@@ -16,17 +15,17 @@ function windowResized() {
 }
 
 function draw() {
-  background(255); // Imposta lo sfondo bianco
+  background(255); // sfondo bianco
 
   translate(width / 2, height / 2);
 
-  let TAU = TWO_PI;  // Definisci TAU come 2π
+  let TAU = TWO_PI;  // 
 
   // Disegna i cerchi centrali
-  stroke(255, 140, 0); // Contorno arancione scuro
+  stroke(255, 0, 255); // Contorno fuchsia fluo
   noFill();
-  circle(0, 0, 700); // Cerchio più esterno
-  circle(0, 0, 600); // Cerchio intermedio
+  circle(0, 0, 680); // Cerchio più esterno
+  circle(0, 0, 560); // Cerchio intermedio
   circle(0, 0, 400); // Cerchio più interno
   circle(0, 0, 200); // Cerchio più interno
   noStroke();
@@ -37,23 +36,23 @@ function draw() {
   }
   lastSecond = second();
 
-  // Calcola la posizione dei secondi
+  // posizione dei secondi
   const ms = (new Date()).getMilliseconds() / 1000 * TAU / 60;
   fill(secondColor);
   push();
   rotate(TAU / 60 * second() + ms);
-  circle(0, -350, 50);
+  circle(0, -310, 60);
   pop();
 
-  // Calcola la posizione dei minuti
+  // posizione dei minuti
   const secondFraction = second() / 60 * TAU / 60;
   fill(minuteColor);
   push();
   rotate(TAU / 60 * minute() + secondFraction);
-  circle(0, -260, 75);
+  circle(0, -240, 80);
   pop();
 
-  // Calcola la posizione delle ore
+  // posizione delle ore
   const minuteFraction = minute() / 60 * TAU / 12;
   fill(hourColor);
   push();
@@ -62,8 +61,9 @@ function draw() {
   pop();
 }
 
+
 function changeColors() {
   secondColor = color(0, 0, 0, 255);  // Nero al 100%
-  minuteColor = color(64, 64, 64, 255);  // Grigio scuro al 100%
+  minuteColor = color(64, 64, 64, 255);  // Grigio scuro al 80%
   hourColor = color(192, 192, 192, 255); // Grigio chiaro al 100%
 }
